b34dd433bc780022f64dd9cb4bcae270c7758097
Berrada Anas <aberrada077@gmail.com>
2022-03-28 22:37:59 +0000

    main commit

---
+++ Push-2-Jira-project/src/test/java/com/pushjira/first/Push2JiraProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pushjira.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Push2JiraProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
